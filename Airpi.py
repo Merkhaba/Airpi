656b724107be6ec22a087d858dbe4b20432edc74
Merkhaba <jerome.disch@laposte.net>
2019-03-24 16:05:54 +0100

    Update Airpi.py

--- Airpi.py
+++ Airpi.py
@@ -155,8 +155,8 @@ if __name__ == '__main__':
     while True:
         l, r ,c = t.read(30)
         temp, hum = Adafruit_DHT.read_retry(sensor,pinDHT)
-        read_pinMQ135 = mq.readadc(0)
-        read_pinMQ2 = mq.readadc(1)
+        read_pinMQ135 = mq.readadc(0) * 3.3 / 1024.0
+        read_pinMQ2 = mq.readadc(1) * 3.3 / 1024.0
         print("[{0}] Temp={3:0.1f}*C  Humidity={4:0.1f}%  Ratio={1:0.6f}  Concentration={2:0>4.2f} pcs per 0.01 cubic foot  MQ135={5}mv  MQ2={6}mv".format(datetime.datetime.now(),r,c,hum,temp,read_pinMQ135,read_pinMQ2))
         time.sleep(15)
     GPIO.cleanup()	
