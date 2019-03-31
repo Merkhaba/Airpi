3eedcbcaf74565fe621e5f4d56170e9ea8be201e
Merkhaba <jerome.disch@laposte.net>
2019-03-31 15:00:30 +0200

    Update Airpi.py

--- Airpi.py
+++ Airpi.py
@@ -276,9 +276,9 @@ if __name__ == '__main__':
     pinDHT = 4
     GPIO.setmode(GPIO.BCM)
     mq = MQAirSensor()
-    //mq135 = MQ135(0) #pin zero for MQ135 on MCP3008
+    #mq135 = MQ135(0) #pin zero for MQ135 on MCP3008
     temp, hum = Adafruit_DHT.read_retry(sensor,pinDHT)
-    //mq135.test(temp,hum)
+    #mq135.test(temp,hum)
     t = Shinyei(27)
     while True:
         l, r ,c = t.read(30)
